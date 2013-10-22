9135274cf1cf27649ff5339e42bfaf5c72aa2de4
cliff777 <bugman12345@gmail.com>
2013-10-21 19:06:49 -0700

    Deity's methods

--- src/com/imdeity/deitydungeons/Cloud.java
+++ src/com/imdeity/deitydungeons/Cloud.java
@@ -1,5 +1,32 @@
 package com.imdeity.deitydungeons;
 
+import org.bukkit.entity.Player;
+
 public class Cloud {
+	//This is for you deity
+
+	public static void onDungeonStart(Player[] players, int dungeonID) {
+
+	}
+
+	public static void onDungeonComplete(Player[] players, int dungeonID) {
+
+	}
+
+	public static void onPlayerJoin(String player) {
+
+	}
+
+	public static void onPlayerDeath(String player) {
+
+	}
+
+	public static void onPlayerRespawn(String player) {
+
+	}
+
+	public static void onPlayerDisconnect(String player) {
+
+	}
 
 }
